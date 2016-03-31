92c3d0bcb667da9031b490c1412c5c402d0bb5eb
yaolei <yaolei313@gmail.com>
2016-03-31 19:34:32 +0800

    part

--- basic/src/main/scala/com/yao/app/MainObject.scala
+++ basic/src/main/scala/com/yao/app/MainObject.scala
@@ -2,23 +2,51 @@ package com.yao.app
 
 import java.io.File
 import java.io.PrintWriter
+import scala.math.Ordered
+
+class Rational(n: Int, d: Int) extends Ordered[Rational] {
+    require(d != 0)
+    private val g = gcd(n, d)
+    val number = n / g
+    val denom = d / g
+    def this(n: Int) = this(n, 1)
+    def +(that: Rational) = new Rational(this.number * that.denom + this.denom * that.number, this.denom * that.denom)
+    def +(i: Int) = new Rational(this.denom * i + this.number, this.denom)
+    def *(that: Rational) = new Rational(this.number * that.number, this.denom * that.denom)
+    def *(i: Int) = new Rational(this.number * i, this.denom)
+
+    override def toString = number + "/" + denom;
+
+    def compare(that: Rational): Int = this.number * that.denom - that.number * this.denom
+
+    // 注意2中equals的区别
+    //def equals(that: Rational): Boolean = (this compare that) == 0
+    override def equals(that: Any): Boolean = {
+        that match {
+            case that1: Rational => (this compare that1) == 0
+            case _               => false
+        }
+    }
+
+    private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+
+}
 
 object MainObject {
     def main(args: Array[String]): Unit = {
+
         println(if (!args.isEmpty) args(0) else "default args")
 
+        def greet() = println("Hello world2")
         println(greet() == ())
 
         //println("hello world");
         //println(max(3,1))
         //println(max2(13,14))
         //greet
-        args.foreach { x => println(x) }
-
         args.foreach { (x: String) => println(x) }
-
+        args.foreach { x => println(x) }
         args.foreach { print }
-
         for (arg <- args)
             println(arg)
 
@@ -44,7 +72,7 @@ object MainObject {
         implicit def intToRational(x: Int) = new Rational(x)
         println(2 * r)
 
-        val pattern = ".*name.*"
+        /*val pattern = ".*name.*"
         val files = (new java.io.File(".")).listFiles
         for (file <- files)
             println(file.getName)
@@ -57,13 +85,52 @@ object MainObject {
         } {
             println(file.getName + ":" + trimed)
             println(file.getPath)
-        }
+        }*/
 
         println("--------------")
-        println(fileLinesLength)
+        //println(fileLinesLength)
 
         val map = Map("123" -> "hello")
         val b23 = map + ("1234" -> "world")
+
+        /**
+         * 借贷模式
+         */
+        def withPrintWriter(file: File)(op: PrintWriter => Unit) {
+            val writer = new PrintWriter(file)
+            try {
+                op(writer)
+            } finally {
+                writer.close()
+            }
+        }
+
+        val file = new File("date.txt");
+        withPrintWriter(file) {
+            writer => writer.print(new java.util.Date())
+        }
+
+        // 传名参数
+        def testCallByName(op: () => Boolean) = if (op()) 1 else 2
+        def testCallByName2(op: => Boolean) = if (op) 1 else 2
+
+        println(testCallByName(() => 1 > 2))
+        println(testCallByName2(1 > 2))
+
+        val t1: Rational = new Rational(10, 1)
+        val t2: Rational = new Rational(6, 1)
+        val t3: Rational = new Rational(6, 1)
+        println(t1 < t2)
+        println(t2 == t3)
+        println(t2 equals t3)
+        //val t2:Int = null null不适用于值类型
+
+        // 标明不正常的终止
+        def error(msg: String): Nothing = throw new RuntimeException(msg)
+
+        def divide(x: Int, y: Int): Int = if (y != 0) x / y else error("123")
+
+        def divide2(x: Int, y: Int): Int = if (y != 0) x / y else throw new RuntimeException("123") //抛出异常的类型是Nothing
     }
 
     def fileLines(file: java.io.File) =
@@ -84,56 +151,5 @@ object MainObject {
 
     def max2(x: Int, y: Int) = if (x > y) x else y
 
-    def greet() = println("Hello world2")
-
-    /**
-     * 借贷模式
-     */
-    def withPrintWriter(file: File)(op: PrintWriter => Unit) {
-        val writer = new PrintWriter(file)
-        try {
-            op(writer)
-        } finally {
-            writer.close()
-        }
-    }
-
-    val file = new File("date.txt");
-    withPrintWriter(file) {
-        writer => writer.print(new java.util.Date())
-    }
-
-    // 传名参数
-    def testCallByName(op: () => Boolean) = if (op()) 1 else 2
-    def testCallByName2(op: => Boolean) = if (op) 1 else 2
-
-    println(testCallByName(() => 1 > 2))
-    println(testCallByName2(1 > 2))
-
-    val t: Rational = null
-    //val t2:Int = null null不适用于值类型
-
-    // 标明不正常的终止
-    def error(msg: String): Nothing = throw new RuntimeException(msg)
-
-    def divide(x: Int, y: Int): Int = if (y != 0) x / y else error("123")
-    
-    def divide2(x: Int, y: Int): Int = if (y != 0) x / y else throw new RuntimeException("123") //抛出异常的类型是Nothing
-
 }
-class Rational(n: Int, d: Int) {
-    require(d != 0)
-    private val g = gcd(n, d)
-    val number = n / g
-    val denom = d / g
-    def this(n: Int) = this(n, 1)
-    def +(that: Rational) = new Rational(this.number * that.denom + this.denom * that.number, this.denom * that.denom)
-    def +(i: Int) = new Rational(this.denom * i + this.number, this.denom)
-    def *(that: Rational) = new Rational(this.number * that.number, this.denom * that.denom)
-    def *(i: Int) = new Rational(this.number * i, this.denom)
 
-    override def toString = number + "/" + denom;
-
-    private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
-
-}
--- basic/src/main/scala/com/yao/app/Test.scala
+++ basic/src/main/scala/com/yao/app/Test.scala
@@ -2,14 +2,27 @@ package com.yao.app
 
 object Test {
     def main(args: Array[String]) {
-        val list1 = List("one","two","three")
-        val list2 = List("hello","world")
-        
+        val list1 = List("one", "two", "three")
+        val list2 = List("hello", "world")
+
         // zip 以最短长度作为新长度
-        val result = for(obj <- list1.zip(list2)) yield{
-            obj._1+obj._2
+        val result = for (obj <- list1.zip(list2)) yield {
+            obj._1 + obj._2
         }
-        
+
         println(result.mkString(","))
+
+        class Test(val id: Int,val name: String)
+
+        def getIds[A, B](l: List[A], op: A => B): List[B] = {
+            for (o <- l)
+                yield op(o)
+        }
+
+        val list = List[Test](new Test(1,"hello"), new Test(2,"world"));
+        val idList = getIds[Test, Int](list, o => o.id)
+        print(idList mkString " ")
+
     }
+
 }
